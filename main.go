e38ade43815118a5b466bb3019a7d8876c547710
ilya.besprozvannykh <ilya.besprozvannykh@redcollar.ru>
2026-08-02 18:21:29 +0300

    feat: add function ScanLogDirectory, ProcessMultipleFiles

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
 	"regexp"
+	"sort"
 	"strings"
 	"time"
 )
@@ -121,14 +124,80 @@ func ReadLogFile(filepath string) ([]LogEntry, error) {
 		return nil, err
 	}
 
-	fmt.Printf("\nProcessing file: %s", filepath)
-	fmt.Println("Total lines:", totalLines)
-	fmt.Println("Valid entries:", len(entries))
-	fmt.Println("Parse errors:", parseErrors)
-
 	return entries, nil
 }
 
+func ScanLogDirectory(dirPath string) ([]string, error) {
+	var logFiles []string
+
+	err := filepath.WalkDir(dirPath, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !d.IsDir() && strings.HasSuffix(path, ".log") {
+			logFiles = append(logFiles, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("walk directory %s: %w", dirPath, err)
+	}
+
+	sort.Strings(logFiles)
+
+	return logFiles, err
+}
+
+func ProcessMultipleFiles(filePaths []string) ([]LogEntry, error) {
+	if len(filePaths) == 0 {
+		return nil, errors.New("file paths is empty")
+	}
+
+	var allEntries []LogEntry
+
+	for _, filePath := range filePaths {
+		entries, err := ReadLogFile(filePath)
+		if err != nil {
+			fmt.Printf("warning: failed to process file %s: %v\n", filePath, err)
+			continue
+		}
+
+		fmt.Printf("%s: %d entries\n", filePath, len(entries))
+
+		allEntries = append(allEntries, entries...)
+	}
+
+	return allEntries, nil
+}
+
 func main() {
-	fmt.Println("Happy coding!!!")
+	dirPath := "./logs"
+
+	fmt.Println("Scanning directory:", dirPath)
+
+	logFiles, err := ScanLogDirectory(dirPath)
+	if err != nil {
+		fmt.Println("error scanning directory:", err)
+		return
+	}
+
+	fmt.Printf("Found %d log files:\n", len(logFiles))
+	for _, file := range logFiles {
+		fmt.Println(" -", file)
+	}
+
+	if len(logFiles) == 0 {
+		fmt.Println("No log files found")
+		return
+	}
+
+	fmt.Println("\nProcessing files...")
+
+	_, err = ProcessMultipleFiles(logFiles)
+	if err != nil {
+		fmt.Println("error processing files:", err)
+		return
+	}
 }
